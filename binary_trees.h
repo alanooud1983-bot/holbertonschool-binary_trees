922779e6eec63d41164d29591ae4627453d8e1f0
Reef Saeed <reefsaeed@hotmail.com>
2025-10-27 12:15:42 +0000

    Add binary_tree_balance function for AVL balance factor

--- binary_trees.h
+++ binary_trees.h
@@ -39,10 +39,13 @@ typedef struct binary_tree_s heap_t;
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 size_t binary_tree_depth(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 #endif
