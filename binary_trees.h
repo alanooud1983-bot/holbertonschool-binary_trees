5ff125c8b690e973b35ec7877b57780068dd96bf
Reef Saeed <reefsaeed@hotmail.com>
2025-10-27 12:02:52 +0000

    Add binary_tree_leaves function for counting tree leaves

--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 size_t binary_tree_depth(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 #endif
